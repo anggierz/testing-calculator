622ada2128f17cfff75823299d0a590a93798317
anggierz <anggie.rizo.zambrano@gmail.com>
2025-06-08 15:56:07 +0200

    add calculator functionality and test

--- src/app/components/calculator/calculator.component.spec.ts
+++ src/app/components/calculator/calculator.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CalculatorComponent } from './calculator.component';
+import { By } from '@angular/platform-browser';
 
 describe('CalculatorComponent', () => {
   let component: CalculatorComponent;
@@ -19,5 +20,15 @@ describe('CalculatorComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+
+  it('should initialize a, b and result with 0', () => {
+    expect(component.a).toBe(0);
+    expect(component.b).toBe(0);
+    expect(component.result).toBe(0);
+
+    const resultadoHtml = fixture.debugElement.query(By.css('h3')).nativeElement;
+    expect(resultadoHtml.textContent).toContain('0');
   });
 });
--- src/app/components/calculator/calculator.component.ts
+++ src/app/components/calculator/calculator.component.ts
@@ -1,12 +1,32 @@
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-calculator',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './calculator.component.html',
   styleUrl: './calculator.component.scss'
 })
 export class CalculatorComponent {
+
+  a: number = 0;
+  b: number = 0;
+  result: number = 0;
+
+  constructor() {}
+
+  sum() {
+    this.result = this.a + this.b;
+  }
+
+  subtract() {
+    this.result = this.a - this.b;
+  }
+
+  multiply() {
+    this.result = this.a * this.b;
+  }
 
 }
